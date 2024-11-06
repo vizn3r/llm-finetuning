c093bbefb49856cce40a15156eee375ec7a4ad9a
Simon Vizner <viznertw@gmail.com>
2024-11-06 20:57:51 +0100

    Updated scape-wiki program, now it has some optional commands

--- scraper/build.sh
+++ scraper/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-echo "Deleting old builds"
-rm -rf ./build/*
 
-go build -o ../build/scrape-wiki wiki.go
+GO_BUILD_DIR=../build
+
+echo "Deleting old builds"
+rm -rf "${GO_BUILD_DIR}/*"
+
+go build -o "${GO_BUILD_DIR}/scrape-wiki" wiki.go
 echo "Building 'scrape-wiki'"
 
 echo "Done"
--- scraper/wiki.go
+++ scraper/wiki.go
@@ -11,14 +11,24 @@ import (
 	"sync"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/spf13/pflag"
 	"golang.org/x/net/html"
 )
 
+type OutputFormat_t int
+
+const (
+	JSON OutputFormat_t = iota
+	JSONL
+)
+
 var (
-	outDir      = "./out"
-	tempDir     = "./temp"
-	datasetName = "dataset"
-	inputLinks  []string
+	outDir        string
+	tempDir       string
+	keepTempFiles bool
+	datasetName   = "dataset"
+	inputLinks    []string
+	outFormat     OutputFormat_t
 
 	crawledLinks sync.Map
 	semaphore    chan struct{}
@@ -31,10 +41,18 @@ func SnakeCase(str string) string {
 type PageData struct {
 	Heading string `json:"heading"`
 	Text    string `json:"text"`
+	Titlee  string `json:"title"`
 }
 
 func WriteData(title string, data []PageData) error {
-	fName := outDir + "/" + title + ".json"
+	var fName string
+	switch outFormat {
+	case JSON:
+		fName = tempDir + "/" + title + ".json"
+	case JSONL:
+		fName = tempDir + "/" + title + ".jsonl"
+	}
+
 	if _, err := os.Stat(fName); !os.IsNotExist(err) {
 		return nil
 	}
@@ -44,9 +62,18 @@ func WriteData(title string, data []PageData) error {
 		return err
 	}
 	defer f.Close()
-
-	encoder := json.NewEncoder(f)
-	return encoder.Encode(data)
+	switch outFormat {
+	case JSON:
+		encoder := json.NewEncoder(f)
+		return encoder.Encode(data)
+	case JSONL:
+		for _, d := range data {
+			jsonData, _ := json.Marshal(d)
+			f.Write(jsonData)
+			f.Write([]byte("\n"))
+		}
+	}
+	return nil
 }
 
 func GetLinks(s *goquery.Selection) []string {
@@ -132,7 +159,7 @@ func Crawl(url string, wg *sync.WaitGroup) {
 
 		if sectionHeading != prevHeading {
 			if section != "" && strings.ToLower(prevHeading) != "see also" {
-				data = append(data, PageData{prevHeading, section})
+				data = append(data, PageData{prevHeading, section, pageTitle})
 			}
 			prevHeading = sectionHeading
 			section = ""
@@ -164,11 +191,11 @@ func Crawl(url string, wg *sync.WaitGroup) {
 	}
 }
 
-func CombineFiles() error {
+func CombineJSONFiles() error {
 	var combinedData []PageData
 
 	// Read all JSON files from the output directory
-	err := filepath.Walk(outDir, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
@@ -193,7 +220,7 @@ func CombineFiles() error {
 	}
 
 	// Marshal the combined data back to JSON
-	outputFile := outDir + "/dataset.json"
+	outputFile := filepath.Join(outDir, datasetName+".json")
 	output, err := json.MarshalIndent(combinedData, "", "  ")
 	if err != nil {
 		return fmt.Errorf("error marshaling combined data: %v", err)
@@ -226,14 +253,61 @@ func CleanDir(dir string) {
 	}
 }
 
+func CombineJSONLFiles() error {
+	var combinedData []byte
+
+	// Read all JSON files from the output directory
+	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		// Process only JSON files
+		if !info.IsDir() && filepath.Ext(path) == ".jsonl" {
+			fileContent, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			combinedData = append(combinedData, fileContent...)
+		}
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("error reading files: %v", err)
+	}
+
+	// Marshal the combined data back to JSON
+	outputFile := filepath.Join(outDir, datasetName+".jsonl")
+
+	// Write the combined JSON data to a file
+	if err := os.WriteFile(outputFile, combinedData, 0644); err != nil {
+		return fmt.Errorf("error writing to output file: %v", err)
+	}
+
+	fmt.Println("Combined JSONL data written to", outputFile)
+	return nil
+}
+
 func main() {
 	wg := new(sync.WaitGroup)
 	semaphore = make(chan struct{}, 50)
 
-	inputLinks = os.Args[1:]
+	p_inputLinks := pflag.StringArray("links", []string{}, "Links to crawl, use format '--links link1 link2 ...'")
+	p_tempDir := pflag.String("temp_dir", "./temp", "Define temporary directory")
+	p_outDir := pflag.String("out_dir", "./out", "Output directory for the data")
+	p_keepTempFiles := pflag.Bool("keep_temp", false, "Keep temporary directory")
+	p_outFormat := pflag.Int("out_format", 0, "Output file format [0 - .json | 1 - .jsonl]")
+
+	pflag.Parse()
+
+	inputLinks = *p_inputLinks
+	tempDir = *p_tempDir
+	outDir = *p_outDir
+	keepTempFiles = *p_keepTempFiles
+	outFormat = OutputFormat_t(*p_outFormat)
 
 	if len(inputLinks) == 0 {
-		fmt.Println("No links provided")
+		fmt.Println("No links provided, use '--links' or '--help'")
 		os.Exit(0)
 	}
 
@@ -251,7 +325,7 @@ func main() {
 	CleanDir(tempDir)
 
 	fmt.Println("Output directory:", outDir)
-	fmt.Println("Dataset name:", datasetName)
+	fmt.Println("Data name:", datasetName)
 	fmt.Println("Scaping wiki")
 
 	// Start crawling
@@ -264,6 +338,24 @@ func main() {
 	}
 	wg.Wait()
 
-	// Delete temorary files after running
-	CleanDir(tempDir)
+	switch outFormat {
+	case JSON:
+		err := CombineJSONFiles()
+		if err != nil {
+			panic(err)
+		}
+	case JSONL:
+		err := CombineJSONLFiles()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Delete temorary files if not keeping
+	if !keepTempFiles {
+		err := os.RemoveAll(tempDir)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
